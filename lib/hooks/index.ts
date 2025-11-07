59948247d1725aad5346e960e86d71d158800138
Ezra <singhadarsh2109@gmail.com>
2025-11-07 06:17:25 +0530

    Create hooks index for easy imports

---
+++ lib/hooks/index.ts
@@ -0,0 +1,6 @@
+// Custom data fetching hooks
+export * from './use-workout-data';
+export * from './use-sleep-data';
+
+// Re-export existing hooks
+export { default as useMobile } from './use-mobile';
